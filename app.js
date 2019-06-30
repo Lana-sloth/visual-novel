4a8a2c6da5e53f5728b2a028445379ad96132453
Svetlana Prokhorova <lana.sloth@gmail.com>
2019-06-30 15:56:05 +0300

    adding choice buttons

--- app.js
+++ app.js
@@ -8,16 +8,47 @@
     function ScenarioService(){
         var service = this;
         var counter = 0;
+        var answer = false;
+        var lastAnswer = '';
 
         service.getName = function(){
             return data[counter].name;
         }
 
         service.getText = function(){
-            return data[counter].text;
+            if (!answer && typeof data[counter].text !== 'string') {
+                return data[counter].text[lastAnswer];
+            }
+            if(!answer || typeof data[counter].text === 'string') {
+                return data[counter].text;
+            }
+            if (answer) {
+                let currentText = data[counter].text[answer];
+                lastAnswer = answer;
+                answer = false;
+                return currentText;
+            }
         }
 
         service.nextLine = function() {
+            if (counter >= data.length - 1 || data[counter].choices || answer) {
+                return;
+            }
+            counter ++;
+        }
+
+        service.getChoices = function(){
+            if (data[counter].choices) {
+                return data[counter].choices;
+            }
+        }
+
+        service.getAnswer = function(){
+            return answer;
+        }
+
+        service.choiceButton = function($index) {
+            answer = data[counter].choices[$index];
             if (counter >= data.length - 1) {
                 return;
             }
@@ -29,9 +60,14 @@
     function TextCtrl(ScenarioService){
         var ctrl = this;
 
-        ctrl.init = function(){
+        ctrl.choiceButton = function($index) {
+            ScenarioService.choiceButton($index);
+        }
+
+        ctrl.init = function() {
             ctrl.name = ScenarioService.getName();
             ctrl.text = ScenarioService.getText();
+            ctrl.choices = ScenarioService.getChoices();
         }
         ctrl.init();
 
--- data.js
+++ data.js
@@ -18,5 +18,70 @@ var data = [
     {
         name: 'Melodie',
         text: 'So glad to see you!'
+    },
+    {
+        name: 'Melodie',
+        text: 'You eh… Wanna tea or something?',
+        choices: [
+            'Tea',
+            'Coffee',
+            'Soda',
+            'Nothing'
+        ]
+    },
+    {
+        name: 'Sunny',
+        text: {
+            Tea: 'Tea would be great.',
+            Coffee: 'Coffee… please…',
+            Soda: 'They have any soda? I’d love some bubbles.',
+            Nothing: 'Nah, I’m good, don’t worry.'
+        }
+    },
+    {
+        name: 'Melodie',
+        text: {
+            Tea: 'Tea it is then!',
+            Coffee: 'Yeah, can’t think strait without it, right? Hahah…',
+            Soda: 'Okay, soda, I got you!',
+            Nothing: 'Well, alrighty then.'
+        }
+    },
+    {
+        name: 'Sunny',
+        text: '...'
+    },
+    {
+        name: 'Sunny',
+        text: 'Melodie, listen...'
+    },
+    {
+        name: 'Melodie',
+        text: 'Oh-oh...'
+    },
+    {
+        name: 'Sunny',
+        text: 'Don\'t "oh-oh" me!' 
+    },
+    {
+        name: 'Sunny',
+        text: {
+            Tea: 'I know it\'s not just a "cup of tea together" thing.',
+            Coffee: 'I know it\'s not just a "cup of coffee together" thing.',
+            Soda: 'I know it\'s not just a "can of soda together" thing.',
+            Nothing: 'I know it\'s not just a "you and me chilling together" thing.'
+        }
+    },
+    {
+        name: 'Sunny',
+        text: 'And yeah, I don\'t have money to buy myself a drink, point taken.'
+    },
+    {
+        name: 'Sunny',
+        text: 'Just...'
+    },
+    {
+        name: 'Sunny',
+        text: 'Just... Tell me what you want to tell.'
     }
 ]
